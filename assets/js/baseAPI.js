506f4e3e1a615b8da5e44402c4f1d2a73f56b407
baileisha <bai_1123@126.com>
2021-09-18 17:53:58 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,25 @@
 // 注意：每次调用$.get()或$.post()或$.ajax()的时候，会先调用$.ajaxPrefilter这个函数，在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function (option) {
-    option.url = 'http://api-breakingnews-web.itheima.net' + option.url;
-    console.log(option.url);
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    console.log(options.url);
+
+    // 统一为有权限的接口设置请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载complete回调函数
+    options.complete = function (res) {
+        // console.log('执行了complete回调：');
+        // console.log(res);
+        // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token');
+            // 2.强制跳转到登陆界面
+            location.href = '/login.html';
+        }
+    }
 })
